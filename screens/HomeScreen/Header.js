474f7dd33531ad49207ded633f2619dd200276aa
Sunaina Asif <u.kulsoom1947@live.com>
2020-02-20 22:34:35 +0500

    header

--- screens/HomeScreen/Header.js
+++ screens/HomeScreen/Header.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
 import {
   StyleSheet,
-  SafeAreaView,
   View,
   Text,
   Image,
+  SafeAreaView,
+  TextInput,
   StatusBar,
-  TouchableOpacity,
-  Alert
+  Alert,
+  TouchableOpacity
 } from "react-native";
+
+import { FontAwesome } from "@expo/vector-icons";
 
 import { moderateScale, scale, verticalScale } from "../../scale";
 import colors from "../../assets/colors";
+
 import { withNavigation } from "react-navigation";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ""
+    };
+  }
+
   tap = () => {
     this.props.navigation.navigate("BrandingDesign");
   };
@@ -24,6 +35,61 @@ class Header extends React.Component {
 
   cart = () => {
     this.props.navigation.navigate("ViewCart");
+  };
+
+  searchbar = () => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colors.bgblue,
+          paddingTop: verticalScale(3),
+          height: verticalScale(50)
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-around",
+            padding: scale(5),
+            marginHorizontal: moderateScale(10),
+            marginBottom: verticalScale(5),
+            backgroundColor: colors.whitetext,
+            width: moderateScale(300),
+            height: verticalScale(40)
+          }}
+        >
+          <Image
+            source={require("../../assets/searchicon.png")}
+            style={{
+              width: moderateScale(25),
+              height: verticalScale(25)
+            }}
+            resizeMode="contain"
+          />
+          <TextInput
+            style={{
+              marginLeft: moderateScale(-20),
+              color: colors.blacktext,
+              fontSize: scale(14),
+              fontWeight: "400"
+            }}
+            value={this.state.search}
+            underlineColorAndroid="transparent"
+            onChangeText={text => this.setState({ search: text })}
+            placeholder={"What are you looking for?"}
+            placeholderTextColor={colors.inactivegreybutton}
+            returnKeyType="done"
+          />
+          <FontAwesome
+            name="microphone"
+            color={colors.inactivegreybutton}
+            size={scale(20)}
+          />
+        </View>
+      </View>
+    );
   };
 
   render() {
@@ -166,6 +232,9 @@ class Header extends React.Component {
             {/* CART ICON END*/}
           </View>
           {/* OTHER ICONS END */}
+        </View>
+        <View>
+          <this.searchbar />
         </View>
       </SafeAreaView>
     );
